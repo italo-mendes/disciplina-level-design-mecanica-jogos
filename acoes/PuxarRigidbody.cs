16b0b346654a10c18cbaa1dd79984a68794b7693
Italo Mendes <65035983+italo-mendes@users.noreply.github.com>
2023-05-31 18:15:21 -0300

    Update and rename Puxar.cs to PuxarRigidbody.cs

--- acoes/PuxarRigidbody.cs
+++ acoes/PuxarRigidbody.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Puxar : MonoBehaviour
+public class PuxarRigidbody : MonoBehaviour
 {
     public string tagObjetoSeraPuxado;
     public float distancia;
