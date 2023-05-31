4102bafdb73dbae070bbb0adf211310b9d6f6646
vitinho28 <64031604+vitinho28@users.noreply.github.com>
2023-05-31 10:15:32 -0300

    Add files via upload

---
+++ Bloco.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blocoCair : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float fallDelay = 2f;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(0.2f);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, fallDelay);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    
+    }
+}
