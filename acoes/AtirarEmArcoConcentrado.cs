b41c44d89c9b8e69e4d5867e00b3a739c454a923
Raylander524 <84639724+Raylander524@users.noreply.github.com>
2023-06-01 15:28:22 -0300

    Add files via upload

---
+++ acoes/AtirarEmArcoConcentrado.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtirarEmArcoConcentrado : MonoBehaviour
+{
+    public GameObject projetil;
+    public int numeroDirecoes;
+    public float tempoEntreDisparos;
+    public float anguloEntreDirecoes;
+    private float angulo;
+    private Quaternion rotacaoInicial;
+
+    void Start()
+    {
+        angulo = anguloEntreDirecoes * numeroDirecoes;
+        rotacaoInicial = transform.localRotation;
+
+        StartCoroutine(Atira());
+    }
+
+    IEnumerator Atira()
+    {
+        while (true)
+        {
+            transform.Rotate(0.0f,  (angulo/ 2)*-1, 0.0f, Space.Self);
+            Instantiate(projetil, transform.position, transform.rotation);
+
+            for (int i = 1; i <= numeroDirecoes - 1; i++)
+            {
+                transform.Rotate(0.0f, anguloEntreDirecoes, 0.0f, Space.Self);
+                Instantiate(projetil, transform.position, transform.rotation);
+            }
+
+            transform.localRotation = rotacaoInicial;
+
+            yield return new WaitForSeconds(tempoEntreDisparos);
+        }
+    }
+}
