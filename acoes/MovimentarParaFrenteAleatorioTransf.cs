5c8091761bb3b3d3af0a275a0de0ecd1e510b7fa
leimarferreira <52300462+leimarferreira@users.noreply.github.com>
2023-06-13 13:59:11 -0300

    Adição do componente MovimentarParaFrenteAleatorioTransf (#21)
    * Add files via upload
    
    * Atualizando componente.
    
    - Utiliza o componente velocidade.
    - Utiliza transform.foward em vez de new Vector3 ao movimentar o GameObject
    
    * Update MovimentarParaFrenteAleatorioTransf.cs
    
    ---------
    
    Co-authored-by: Italo Mendes <65035983+italo-mendes@users.noreply.github.com>

---
+++ acoes/MovimentarParaFrenteAleatorioTransf.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentarParaFrenteAleatorioTransf : MonoBehaviour
+{
+    private Velocidade velocidade;
+    [SerializeField]
+    private float tempoDeMovimentacao;
+    [SerializeField]
+    private float anguloMin;
+    [SerializeField]
+    private float anguloMax;
+
+    void Start()
+    {
+        if (!TryGetComponent<Velocidade>(out velocidade))
+            print("Adicione o componente <color=orange>Velocidade</color> ao GameObject.");
+            
+        StartCoroutine(Movimentar());
+    }
+
+    IEnumerator Movimentar()
+    {
+        while (true)
+        {
+            float total = 0.0f;
+            while (total <= tempoDeMovimentacao)
+            {
+                transform.Translate(velocidade.GetVelocidade() * Time.deltaTime * transform.forward);
+                total += Time.deltaTime;
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            float angulo = Random.Range(anguloMin, anguloMax);
+            transform.Rotate(0.0f, angulo, 0.0f);
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
