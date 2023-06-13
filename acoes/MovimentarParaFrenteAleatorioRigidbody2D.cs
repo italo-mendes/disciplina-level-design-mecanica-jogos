e261c5f59ee202a1da9828dae2ab5144802aef43
leimarferreira <52300462+leimarferreira@users.noreply.github.com>
2023-06-13 14:12:36 -0300

    Adição do componente MovimentarParaFrenteAleatorioRigidbody2D (#24)
    * Adição do componente MovimentarParaFrenteAleatorioRigidbody2D
    
    * Atualizando o componente.
    
    - Utiliza o componente velocidade.
    
    * Update MovimentarParaFrenteAleatorioRigidbody2D.cs
    
    ---------
    
    Co-authored-by: Italo Mendes <65035983+italo-mendes@users.noreply.github.com>

---
+++ acoes/MovimentarParaFrenteAleatorioRigidbody2D.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentarParaFrenteAleatorioRigidbody2D : MonoBehaviour
+{
+    private Velocidade velocidade;
+    [SerializeField]
+    private float tempoDeMovimentacao;
+    [SerializeField]
+    private float anguloMin;
+    [SerializeField]
+    private float anguloMax;
+
+    private Rigidbody2D rb;
+    private bool movendo;
+
+    void Start()
+    {
+        if (!TryGetComponent<Velocidade>(out velocidade))
+            print("Adicione o componente <color=orange>Velocidade</color> ao GameObject.");
+            
+        rb = GetComponent<Rigidbody2D>();
+        movendo = false;
+
+        StartCoroutine(Movimentar());
+    }
+
+    void FixedUpdate()
+    {
+        if (movendo)
+        {
+            rb.MovePosition(
+                velocidade.GetVelocidade() * Time.fixedDeltaTime
+                * (Vector2)transform.right + rb.position);
+        }
+        else
+        {
+            float angulo = Random.Range(anguloMin, anguloMax);
+            rb.MoveRotation(rb.rotation + angulo);
+        }
+    }
+
+    IEnumerator Movimentar()
+    {
+        while (true)
+        {
+            movendo = true;
+            yield return new WaitForSeconds(tempoDeMovimentacao);
+            movendo = false;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
