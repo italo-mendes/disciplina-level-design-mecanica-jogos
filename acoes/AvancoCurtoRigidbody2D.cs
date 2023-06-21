be2acc3aeb8dfbf14b09f62dcc96d9deae38bfb5
Gabriel Rudan <84931636+gabrielrudan@users.noreply.github.com>
2023-06-21 10:13:21 -0300

    Adicionando o componente Avanço Curto 2D

---
+++ acoes/AvancoCurtoRigidbody2D.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvancoCurtoRigidbody2D : MonoBehaviour
+{
+
+    [SerializeField]
+    private float distanciaDoAvanco;
+    [SerializeField]
+    private float velocidadeDoAvanco;
+
+    private Vector3 eixosDeAvanco;
+
+    [SerializeField]
+    private bool avancarNoEixoX;
+    [SerializeField]
+    private bool avancarNoEixoY;
+    [SerializeField]
+    private bool avancarNoEixoZ;
+
+    private Rigidbody2D rb2D;
+    
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(Avancar());
+            
+    }
+
+    IEnumerator Avancar()
+    {
+        float dist = distanciaDoAvanco;
+
+        if (!avancarNoEixoX & !avancarNoEixoY & !avancarNoEixoZ)
+        {
+            print("Selecione ao menos um dos eixos para o qual o objeto avançará");
+        }
+        else
+        {
+            if (avancarNoEixoX)
+            {
+                eixosDeAvanco.x = 0.25f;
+            }
+
+            if (avancarNoEixoY)
+            {
+                eixosDeAvanco.y = 0.25f;
+            }
+
+            if (avancarNoEixoZ)
+            {
+                eixosDeAvanco.z = 0.25f;
+            }
+
+            while (dist > 0)
+            {
+                rb2D.MovePosition(transform.position + velocidadeDoAvanco
+                    * Time.deltaTime * eixosDeAvanco);
+
+                dist -= Time.deltaTime * velocidadeDoAvanco;
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+        }
+    }
+}
