fb2ed7a599af8033e92b650128041be649c10191
Italo Mendes <65035983+italo-mendes@users.noreply.github.com>
2023-06-23 16:51:18 -0300

    Update AtirarEmArcoConcentrado.cs

--- acoes/AtirarEmArcoConcentrado.cs
+++ acoes/AtirarEmArcoConcentrado.cs
@@ -9,11 +9,13 @@ public class AtirarEmArcoConcentrado : MonoBehaviour
     public float tempoEntreDisparos;
     public float anguloEntreDirecoes;
     private float angulo;
+    private float anguloTotal;
     private Quaternion rotacaoInicial;
 
     void Start()
     {
-        angulo = anguloEntreDirecoes * numeroDirecoes;
+        anguloTotal = anguloEntreDirecoes * (numeroDirecoes - 1);
+        angulo = anguloTotal / numeroDirecoes;
         rotacaoInicial = transform.localRotation;
 
         StartCoroutine(Atira());
@@ -23,7 +25,7 @@ public class AtirarEmArcoConcentrado : MonoBehaviour
     {
         while (true)
         {
-            transform.Rotate(0.0f,  (angulo/ 2)*-1, 0.0f, Space.Self);
+            transform.Rotate(0.0f, -((anguloTotal/2) - (angulo / 2)), 0.0f, Space.Self);
             Instantiate(projetil, transform.position, transform.rotation);
 
             for (int i = 1; i <= numeroDirecoes - 1; i++)
