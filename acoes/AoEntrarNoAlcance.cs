cf249ce4624af246fb932ebd73641b18f9df6e26
Leticia Torres <78484018+leh-torres@users.noreply.github.com>
2023-06-23 17:27:49 -0300

    Componente AoEntrarNoAlcance finalizado (#39)
    * Componente AoEntrarNoAlcance finalizado
    
    * Update AoEntrarNoAlcance.cs
    
    ---------
    
    Co-authored-by: Italo Mendes <65035983+italo-mendes@users.noreply.github.com>

---
+++ acoes/AoEntrarNoAlcance.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AoEntrarNoAlcance : MonoBehaviour
+{
+    public string tagDoObjetoQueEntrarEmContato;
+    public UnityEvent evento;
+
+    /**Código feito para 2d*/
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(tagDoObjetoQueEntrarEmContato))
+        {
+            evento.Invoke();
+        }
+    }
+
+    /**Código feito para 3d*/
+    /**private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tagDoObjetoQueEntrarEmContato))
+        {
+            evento.Invoke();
+        }
+    }*/
+}
