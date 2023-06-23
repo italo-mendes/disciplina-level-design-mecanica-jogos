68551836a96947e1855bde1a2daf8edb6c8c5784
Italo Mendes <65035983+italo-mendes@users.noreply.github.com>
2023-06-23 17:27:37 -0300

    Update AoEntrarNoAlcance.cs

--- acoes/AoEntrarNoAlcance.cs
+++ acoes/AoEntrarNoAlcance.cs
@@ -5,12 +5,10 @@ using UnityEngine.Events;
 
 public class AoEntrarNoAlcance : MonoBehaviour
 {
-    
-
     public string tagDoObjetoQueEntrarEmContato;
     public UnityEvent evento;
 
-    /**Código feito para 2d*/
+    /**CÃ³digo feito para 2d*/
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag(tagDoObjetoQueEntrarEmContato))
@@ -19,7 +17,7 @@ public class AoEntrarNoAlcance : MonoBehaviour
         }
     }
 
-    /**Código feito para 3d*/
+    /**CÃ³digo feito para 3d*/
     /**private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(tagDoObjetoQueEntrarEmContato))
