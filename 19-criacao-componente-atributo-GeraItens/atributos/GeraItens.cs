ee72f6f0b56a51f602bb561aae4fcac1bb076ceb
André Farias <andrefarias@alu.ufc.br>
2023-07-10 12:42:36 -0300

    Add files via upload

---
+++ 19-criacao-componente-atributo-GeraItens/atributos/GeraItens.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GeraItens : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public int quantidadeSerGerada;
+    public float intervaloDeTempo;
+    public bool seraDestruido;
+
+    public void Start()
+    {
+        StartCoroutine(GerarObjetos());
+    }
+
+    public IEnumerator GerarObjetos()
+    {
+        while (quantidadeSerGerada == 0 || quantidadeSerGerada > 0)
+        {
+            Instantiate(itemPrefab, transform.position, transform.rotation);
+
+            if (quantidade > 0)
+                quantidade--;
+
+            if (quantidade == 0 && seraDestruido)
+            {
+                Destroy(gameObject);
+                yield break;
+            }
+
+            yield return new WaitForSeconds(intervaloDeTempo);
+        }
+    }
+}
