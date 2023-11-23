db4c987e940da44ed94f49068fb16937176ace6f
Italo Mendes <65035983+italo-mendes@users.noreply.github.com>
2023-11-23 15:38:56 -0300

    Add files via upload

--- acoes/MovimentarParaAlvo2D.cs
+++ acoes/MovimentarParaAlvo2D.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimentarParaAlvo : MonoBehaviour
+public class MovimentarParaAlvo2D : MonoBehaviour
 {
     public Transform alvo;
     public float distanciaDeParada;
     private Velocidade velocidadeComponent;
     private Rigidbody rb;
+    private Rigidbody2D rb2D;
 
     void Start()
     {
@@ -15,18 +16,30 @@ public class MovimentarParaAlvo : MonoBehaviour
             print("Adicione o componente <color=orange>Velocidade</color> ao GameObject.");
 
         rb = GetComponent<Rigidbody>();
+        rb2D = GetComponent<Rigidbody2D>();
 
         if (rb != null)
             StartCoroutine(MovimentaRigidbody());
+        else if (rb2D != null)
+            StartCoroutine(MovimentaRigidbody2D());
         else
             StartCoroutine(MovimentaTransform());
+    }
+
+    void LookAt2D()
+    {
+        Vector3 direcao = alvo.position - transform.position;
+        direcao.Normalize();
+
+        float rotacaoZ = Mathf.Atan2(direcao.y, direcao.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotacaoZ - 90);
     }
 
     IEnumerator MovimentaTransform()
     {
         while (true)
         {
-            transform.LookAt(alvo);
+            LookAt2D();
 
             transform.position = Vector3.MoveTowards(
                 transform.position, alvo.position, 
@@ -43,9 +56,25 @@ public class MovimentarParaAlvo : MonoBehaviour
     {
         while (true)
         {
-            transform.LookAt(alvo);
+            LookAt2D();
 
             rb.MovePosition(transform.position + velocidadeComponent.GetVelocidade()
+                    * Time.deltaTime * (alvo.position - transform.position));
+
+            if (Vector3.Distance(transform.position, alvo.position) <= distanciaDeParada)
+                yield break;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator MovimentaRigidbody2D()
+    {
+        while (true)
+        {
+            LookAt2D();
+
+            rb2D.MovePosition(transform.position + velocidadeComponent.GetVelocidade()
                     * Time.deltaTime * (alvo.position - transform.position));
 
             if (Vector3.Distance(transform.position, alvo.position) <= distanciaDeParada)
