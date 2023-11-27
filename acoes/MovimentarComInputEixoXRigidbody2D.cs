7a5d1aa2676686d4e660f89159930c6b203ee29f
Italo Mendes <65035983+italo-mendes@users.noreply.github.com>
2023-11-27 15:02:24 -0300

    Add files via upload

--- acoes/MovimentarComInputEixoXRigidbody2D.cs
+++ acoes/MovimentarComInputEixoXRigidbody2D.cs
@@ -12,6 +12,7 @@ public class MovimentarComInputEixoXRigidbody2D : MonoBehaviour
     private Animator animator;
     public string nomeParametroVelocidade;
     private Rigidbody2D rb2D;
+    private PlayerInput pi;
 
     void Start()
     {
@@ -21,6 +22,10 @@ public class MovimentarComInputEixoXRigidbody2D : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
+        pi = GetComponent<PlayerInput>();
+
+        if (pi == null)
+            StartCoroutine(ObtemEntradaEixos());
     }
 
     public void ObtemEntrada(InputAction.CallbackContext contexto)
@@ -39,7 +44,32 @@ public class MovimentarComInputEixoXRigidbody2D : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb2D.MovePosition(transform.position + velocidadeComponent.GetVelocidade()
-            * entrada.x * Time.fixedDeltaTime * transform.right);
+        //rb2D.MovePosition(transform.position + velocidadeComponent.GetVelocidade()
+        //    * entrada.x * Time.fixedDeltaTime * transform.right);
+
+        //rb2D.AddForce(velocidadeComponent.GetVelocidade()
+        //    * entrada.x * Time.fixedDeltaTime * transform.right);
+
+        rb2D.velocity = new Vector2(velocidadeComponent.GetVelocidade()
+            * entrada.x * Time.fixedDeltaTime, rb2D.velocity.y);
+    }
+
+    IEnumerator ObtemEntradaEixos()
+    {
+        while (true)
+        {
+            entrada.x = Input.GetAxisRaw("Horizontal");
+
+            if (animator != null)
+                animator.SetFloat(nomeParametroVelocidade, Mathf.Abs(entrada.x));
+
+            if (sr != null && viraSprite)
+                if (entrada.x < 0)
+                    sr.flipX = true;
+                else if (entrada.x > 0)
+                    sr.flipX = false;
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
